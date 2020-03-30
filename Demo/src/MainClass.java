4d5a8bf67d84065a0e21ce69222fe94e7fa6867f
Yuvaraj Mohite <yuvaraj.mohite@.com>
2020-03-30 11:05:34 +0530

    Commiting the main class

---
+++ Demo/src/MainClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Yuvaraj Mohite
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
